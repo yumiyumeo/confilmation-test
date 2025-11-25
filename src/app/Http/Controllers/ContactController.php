ee62e2fd7fda699bf0e15a94aedc1f2aa3643ac6
yumiyumeo <yuuki0523f@gmail.com>
2025-11-25 10:03:26 +0900

    COACHTECH　確認テスト

---
+++ src/app/Http/Controllers/ContactController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Contact;
+use App\Http\Requests\ContactRequest;
+
+class ContactController extends Controller
+{
+    public function index()
+    {
+    return view('index');
+    }
+
+    public function confirm(ContactRequest $request)
+    {
+        $contact = $request->only(['name', 'email', 'tel', 'content']);
+        return view('confirm', ['contact' => $contact]);
+    }
+
+    public function store(ContactRequest $request)
+    {
+        $contact = $request->only(['name', 'email', 'tel', 'content']);
+        Contact::create($contact);
+        return view('thanks');
+    }
+}
